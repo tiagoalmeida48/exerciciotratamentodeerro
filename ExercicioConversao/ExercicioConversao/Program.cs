a5bcbc68a41a1f3def4895810c45add52f730d89
Tiago Almeida <53452661+tiagoalmeida48@users.noreply.github.com>
2021-08-27 20:24:25 -0300

    Add files via upload

---
+++ ExercicioConversao/ExercicioConversao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicioConversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+
+            try
+            {
+                Console.Write("Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("Não foi possivel converter");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Final da execução do programa");
+            }
+        }
+    }
+}
